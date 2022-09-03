b07504fdc7b326cc0b30f36b7d56a856f1b411ea
gloryofchina <109717157+gloryofchina@users.noreply.github.com>
2022-09-03 08:34:41 +0800

    Update FB.js

--- FB.js
+++ FB.js
@@ -1,36 +1,39 @@
 {
-  "request_date":"2022-08-21T02:31:40Z",
-  "request_date_ms":1661049100189,
-  "subscriber":{
-    "entitlements":{
-      "filebox_pro":{
-        "expires_date":null,
-        "grace_period_expires_date":null,
-        "product_identifier":"filebox_pro",
-        "purchase_date":"2022-08-19T01:38:47Z"
-      }
-    },
-    "first_seen":"2022-08-18T23:00:12Z",
-    "last_seen":"2022-08-20T11:46:01Z",
-    "management_url":null,
-    "non_subscriptions":{
-      "filebox_pro":[
+  "request_date_ms" : 416505600000,
+  "request_date" : "1983-03-15T00:00:00Z",
+  "subscriber" : {
+    "non_subscriptions" : {
+      "filebox_pro" : [
         {
-          "id":"a5bc67b5d6",
-          "is_sandbox":false,
-          "original_purchase_date":"2022-08-19T01:38:47Z",
-          "purchase_date":"2022-08-19T01:38:47Z",
-          "store":"app_store"
-        }]
+          "id" : "chxm1023",
+          "is_sandbox" : false,
+          "purchase_date" : "1983-03-15T00:00:00Z",
+          "original_purchase_date" : "1983-03-15T00:00:00Z",
+          "store" : "app_store"
+        }
+      ]
     },
-    "original_app_user_id":"$RCAnonymousID:a4c87ef7204b451f96ee13762fd7b2f2",
-    "original_application_version":"102",
-    "original_purchase_date":"2022-02-20T05:07:45Z",
-    "other_purchases":{
-      "filebox_pro":{
-        "purchase_date":"2022-08-19T01:38:47Z"
+    "first_seen" : "1983-03-15T00:00:00Z",
+    "original_application_version" : "162",
+    "other_purchases" : {
+      "filebox_pro" : {
+        "purchase_date" : "1983-03-15T00:00:00Z"
       }
     },
-    "subscriptions":{
-    }
+    "management_url" : null,
+    "subscriptions" : {
+
+    },
+    "entitlements" : {
+      "filebox_pro" : {
+        "grace_period_expires_date" : null,
+        "purchase_date" : "1983-03-15T00:00:00Z",
+        "product_identifier" : "filebox_pro",
+        "expires_date" : "null"
+      }
+    },
+    "original_purchase_date" : "1983-03-15T00:00:00Z",
+    "original_app_user_id" : "$RCAnonymousID:88af1c116b274f27aaec084a31ebe80f",
+    "last_seen" : "1983-03-15T00:00:00Z"
   }
+}

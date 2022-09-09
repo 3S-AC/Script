23f07646862dc3af5cdb41057b0326cb6ca0c3e2
gloryofchina <109717157+gloryofchina@users.noreply.github.com>
2022-09-09 14:47:16 +0800

    Update FB.js

--- FB.js
+++ FB.js
@@ -1,39 +1,17 @@
-{
-  "request_date_ms" : 416505600000,
-  "request_date" : "1983-03-15T00:00:00Z",
-  "subscriber" : {
-    "non_subscriptions" : {
-      "filebox_pro" : [
-        {
-          "id" : "chxm1023",
-          "is_sandbox" : false,
-          "purchase_date" : "1983-03-15T00:00:00Z",
-          "original_purchase_date" : "1983-03-15T00:00:00Z",
-          "store" : "app_store"
-        }
-      ]
-    },
-    "first_seen" : "1983-03-15T00:00:00Z",
-    "original_application_version" : "169",
-    "other_purchases" : {
-      "filebox_pro" : {
-        "purchase_date" : "1983-03-15T00:00:00Z"
-      }
-    },
-    "management_url" : null,
-    "subscriptions" : {
+var head = $request.headers;
+var ua = head['User-Agent'];
 
-    },
-    "entitlements" : {
-      "filebox_pro" : {
-        "grace_period_expires_date" : null,
-        "purchase_date" : "1983-03-15T00:00:00Z",
-        "product_identifier" : "filebox_pro",
-        "expires_date" : "2999-03-15T00:00:00Z"
-      }
-    },
-    "original_purchase_date" : "1983-03-15T00:00:00Z",
-    "original_app_user_id" : "$RCAnonymousID:88af1c116b274f27aaec084a31ebe80f",
-    "last_seen" : "1983-03-15T00:00:00Z"
-  }
+if (ua.indexOf('Fileball') != -1) {
+    Body = {"request_date":"1983-03-15T00:00:00Z","request_date_ms":416505600000,"subscriber":{"entitlements":{"filebox_pro":{"expires_date":null,"grace_period_expires_date":null,"product_identifier":"filebox_pro","purchase_date":"1983-03-15T00:00:00Z"}},"first_seen":"1983-03-15T00:00:00Z","last_seen":"1983-03-15T00:00:00Z","management_url":null,"non_subscriptions":{"filebox_pro":[{"id":"yingzi","is_sandbox":false,"original_purchase_date":"1983-03-15T00:00:00Z","purchase_date":"1983-03-15T00:00:00Z","store":"app_store"}]},"original_app_user_id":"$RCAnonymousID%3A8e5a11e56b4246f2ab2b17058c01db1e","original_application_version":"170","original_purchase_date":"1983-03-15T00:00:00Z","other_purchases":{"filebox_pro":{"purchase_date":"1983-03-15T00:00:00Z"}},"subscriptions":{}}};
 }
+
+Status = 'HTTP/1.1 200 OK';
+Headers = {"Content-Type": "application/json"};
+
+const Response = {
+    status: Status,
+    headers: Headers,
+    body: JSON.stringify(Body)
+};
+
+$done(Response);

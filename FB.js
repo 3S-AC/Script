01cd8bb956ee006d1e3efa2a197c5e1614fad9b4
gloryofchina <109717157+gloryofchina@users.noreply.github.com>
2022-09-12 14:07:21 +0800

    Update FB.js

--- FB.js
+++ FB.js
@@ -1,24 +1,11 @@
-/******************************
-脚本功能：FB解锁
-使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
-*******************************
-[rewrite_local]
-^https:\/\/api\.revenuecat\.com\/v1\/subscribers.+ url script-response-body https://raw.githubusercontent.com/gloryofchina/Script/main/FB.js
-[mitm] 
-hostname = api.revenuecat.com
-*******************************/
-
-var body = $response.body;
-var objk = JSON.parse(body);
-
-objk = {
+{
   "request_date_ms" : 416505600000,
   "request_date" : "1983-03-15T00:00:00Z",
   "subscriber" : {
     "non_subscriptions" : {
       "filebox_pro" : [
         {
-          "id" : "cs",
+          "id" : "chxm1023",
           "is_sandbox" : false,
           "purchase_date" : "1983-03-15T00:00:00Z",
           "original_purchase_date" : "1983-03-15T00:00:00Z",
@@ -27,13 +14,13 @@ objk = {
       ]
     },
     "first_seen" : "1983-03-15T00:00:00Z",
-    "original_application_version" : "170",
+    "original_application_version" : "162",
     "other_purchases" : {
       "filebox_pro" : {
         "purchase_date" : "1983-03-15T00:00:00Z"
       }
     },
-    "management_url" : "null",
+    "management_url" : null,
     "subscriptions" : {
 
     },
@@ -42,16 +29,11 @@ objk = {
         "grace_period_expires_date" : null,
         "purchase_date" : "1983-03-15T00:00:00Z",
         "product_identifier" : "filebox_pro",
-        "expires_date" : "null"
+        "expires_date" : "2999-03-15T00:00:00Z"
       }
     },
     "original_purchase_date" : "1983-03-15T00:00:00Z",
-    "original_app_user_id" : "$RCAnonymousID%3A8e5a11e56b4246f2ab2b17058c01db1e",
+    "original_app_user_id" : "$RCAnonymousID:88af1c116b274f27aaec084a31ebe80f",
     "last_seen" : "1983-03-15T00:00:00Z"
   }
 }
-
-
-body = JSON.stringify(objk);
-
-$done({body});

bcad079eddca57a1e7aa0721f38333c704429118
gloryofchina <109717157+gloryofchina@users.noreply.github.com>
2022-09-12 13:47:48 +0800

    Update FB.js

--- FB.js
+++ FB.js
@@ -1,17 +1,57 @@
-var head = $request.headers;
-var ua = head['User-Agent'];
+/******************************
+脚本功能：FB解锁
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+*******************************
+[rewrite_local]
+^https:\/\/api\.revenuecat\.com\/v1\/subscribers.+ url script-response-body https://raw.githubusercontent.com/gloryofchina/Script/main/FB.js
+[mitm] 
+hostname = api.revenuecat.com
+*******************************/
 
-if (ua.indexOf('Fileball') != -1) {
-    Body = {"request_date":"1983-03-15T00:00:00Z","request_date_ms":416505600000,"subscriber":{"entitlements":{"filebox_pro":{"expires_date":null,"grace_period_expires_date":null,"product_identifier":"filebox_pro","purchase_date":"1983-03-15T00:00:00Z"}},"first_seen":"1983-03-15T00:00:00Z","last_seen":"1983-03-15T00:00:00Z","management_url":null,"non_subscriptions":{"filebox_pro":[{"id":"yingzi","is_sandbox":false,"original_purchase_date":"1983-03-15T00:00:00Z","purchase_date":"1983-03-15T00:00:00Z","store":"app_store"}]},"original_app_user_id":"$RCAnonymousID%3A8e5a11e56b4246f2ab2b17058c01db1e","original_application_version":"170","original_purchase_date":"1983-03-15T00:00:00Z","other_purchases":{"filebox_pro":{"purchase_date":"1983-03-15T00:00:00Z"}},"subscriptions":{}}};
+var body = $response.body;
+var objk = JSON.parse(body);
+
+objk = {
+  "request_date_ms" : 416505600000,
+  "request_date" : "1983-03-15T00:00:00Z",
+  "subscriber" : {
+    "non_subscriptions" : {
+      "filebox_pro" : [
+        {
+          "id" : "cs",
+          "is_sandbox" : false,
+          "purchase_date" : "1983-03-15T00:00:00Z",
+          "original_purchase_date" : "1983-03-15T00:00:00Z",
+          "store" : "app_store"
+        }
+      ]
+    },
+    "first_seen" : "1983-03-15T00:00:00Z",
+    "original_application_version" : "170",
+    "other_purchases" : {
+      "filebox_pro" : {
+        "purchase_date" : "1983-03-15T00:00:00Z"
+      }
+    },
+    "management_url" : "null",
+    "subscriptions" : {
+
+    },
+    "entitlements" : {
+      "filebox_pro" : {
+        "grace_period_expires_date" : null,
+        "purchase_date" : "1983-03-15T00:00:00Z",
+        "product_identifier" : "filebox_pro",
+        "expires_date" : "null"
+      }
+    },
+    "original_purchase_date" : "1983-03-15T00:00:00Z",
+    "original_app_user_id" : "$RCAnonymousID%3A8e5a11e56b4246f2ab2b17058c01db1e",
+    "last_seen" : "1983-03-15T00:00:00Z"
+  }
 }
 
-Status = 'HTTP/1.1 200 OK';
-Headers = {"Content-Type": "application/json"};
 
-const Response = {
-    status: Status,
-    headers: Headers,
-    body: JSON.stringify(Body)
-};
+body = JSON.stringify(objk);
 
-$done(Response);
+$done({body});

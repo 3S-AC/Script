df1944695b6d8e3ce0173f45d39f44f37878322c
3S-AC <109717157+3S-AC@users.noreply.github.com>
2024-01-15 13:46:57 +0800

    Delete Aliyun.js

--- Aliyun.js
+++
@@ -1,5 +0,0 @@
-let cs = JSON.parse($response.body);
-if (cs.result) {
-    cs.result = Object.values(cs.result).filter(item => (item["appCode"]=="file" || item["appCode"]=="video"));
-}
-$done({ body: JSON.stringify(cs) });
